324e131606c32699e39bec572224b4dcb62e9280
Thanadon Mahaparn <thanadon.mhp@gmail.com>
2025-01-20 17:55:44 +0700

    Integrate React into project.

--- Jewelry-Cattalog/resources/js/app.js
+++ Jewelry-Cattalog/resources/js/app.js
@@ -1,1 +1,11 @@
 import './bootstrap';
+
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+// Example Component
+function App() {
+    return <h1>Hello, React in Laravel with Sail!</h1>;
+}
+
+ReactDOM.createRoot(document.getElementById('app')).render(<App />);
