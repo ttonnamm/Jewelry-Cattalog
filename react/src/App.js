c4d11451f84a2fb295f9057417161789c6c9ca63
Jirathiphorn Rattanabut <151444848+SerFresh@users.noreply.github.com>
2025-01-22 18:33:24 +0700

    Update App.js

--- react/src/App.js
+++ react/src/App.js
@@ -1,46 +1,69 @@
 import './App.css';
-import ProductItem from './ProductItem.js';
-import { useState } from 'react';
+import React, { useState } from "react";
+import ProductItem from './ProductItem';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import ProductDetail from "./ProductDetail"; // หน้ารายละเอียดสินค้า
 
 const products = [
-    {
-        id: 1,
-        name:"Sakuma",
-        type:"A",
-        image:"Sakuma.png",
-        price:"21,000"
+    { 
+        id: 1, //ProductItem line-> 10 | ProductDetail line-> 7
+        name: "Sakuma", //ProductItem line-> 12,14 | ProductDetail line-> 16,18
+        type: "A", //ProductItem line-> 10 
+        material: "silver", //ProductItem line-> 10
+        image: "images/Sakuma.png", //ProductItem line-> 12 | ProductDetail line-> 16
+        price: "21,000", //ProductItem line-> 15 | ProductDetail line-> 19
+        detail: "qwertyuiopasdfghjklzxcvbnm," // ProductDetail line-> 20
     },
     {
         id: 2,
-        name:"Test",
-        type:"B",
-        image:"testimg.jpg",
-        price:"32,000"
+        name: "Test",
+        type: "B",
+        material: "gold",
+        image: "images/testimg.jpg",
+        price: "32,000",
+        detail: "741852963789456123369258147"
     }
-]
+];
 
 function App() {
     const [filterA, setFilterA] = useState(false);
     const [filterB, setFilterB] = useState(false);
     const [filterC, setFilterC] = useState(false);
+    const [filterD, setFilterD] = useState(false);
+    const [silver, setFiltersilver] = useState(false);
+    const [gold, setFiltergold] = useState(false);
 
     const handleFilterChange = (filter) => {
         if (filter === 'A') {
             setFilterA(!filterA);
             setFilterB(false);
             setFilterC(false);
-            
+            setFilterD(false);
         } else if (filter === 'B') {
             setFilterB(!filterB);
             setFilterA(false);
             setFilterC(false);
-            
+            setFilterD(false);
         } else if (filter === 'C') {
             setFilterC(!filterC);
             setFilterA(false);
             setFilterB(false);
-            
+            setFilterD(false);
+        } else if (filter === 'D') {
+            setFilterD(!filterD);
+            setFilterA(false);
+            setFilterB(false);
+            setFilterC(false)
         }
+    };
+    const handleFilterMaterial = (filter) => {
+        if (filter === 'silver') {
+            setFiltersilver(!silver);
+            setFiltergold(false);
+        } else if (filter === 'gold') {
+            setFiltergold(!gold);
+            setFiltersilver(false);
+        } 
     };
 
     // กรองสินค้าเพื่อแสดงตามการเลือกประเภท
@@ -48,13 +71,12 @@ function App() {
         if (filterA) return product.type === 'A';
         if (filterB) return product.type === 'B';
         if (filterC) return product.type === 'C';
+        if (filterD) return product.type === 'D';
+        if (silver) return product.material === 'silver';
+        if (gold) return product.material === 'gold';
         return true; // แสดงสินค้าทั้งหมดหากไม่ได้เลือกประเภทใดๆ
     });
 
-    // แสดงรายการสินค้าโดยการกรอง
-    const productElements = filteredProducts.map((product, index) => (
-        <ProductItem key={index} product={product} />
-    ));
     return (
         <div className="App">
             <aside>
@@ -89,33 +111,68 @@ function App() {
                     <label className="cbx" htmlFor="filterC"></label>
                     <label className="lbl" htmlFor="filterC">C</label>
                 </div>
+                <div className="checkbox-wrapper-42">
+                    <input 
+                        id="filterD" 
+                        type="checkbox" 
+                        checked={filterD} 
+                        onChange={() => handleFilterChange('D')} 
+                    />
+                    <label className="cbx" htmlFor="filterD"></label>
+                    <label className="lbl" htmlFor="filterD">D</label>
+                </div>
+                <h3>Material</h3>
+                <div className="checkbox-wrapper-42">
+                    <input 
+                        id="silver" 
+                        type="checkbox" 
+                        checked={silver} 
+                        onChange={() => handleFilterMaterial('silver')} 
+                    />
+                    <label className="cbx" htmlFor="silver"></label>
+                    <label className="lbl" htmlFor="silver">Silver</label>
+                </div>
+                <div className="checkbox-wrapper-42">
+                    <input 
+                        id="gold" 
+                        type="checkbox" 
+                        checked={gold} 
+                        onChange={() => handleFilterMaterial('gold')} 
+                    />
+                    <label className="cbx" htmlFor="gold"></label>
+                    <label className="lbl" htmlFor="gold">Gold</label>
+                </div>
             </aside>
+            
             <main>
                 <div>
                     <div className="visibility">
-                        <div className="product-image"></div>
-                        <p className="product-name"> </p>
-                        <p className="product-price"> </p>
+                        <div class="product-image"></div>
+                        <p class="product-name"> </p>
+                        <p class="product-price"> </p>
                     </div>
                     <div className="visibility">
-                        <div className="product-image"></div>
-                        <p className="product-name"> </p>
-                        <p className="product-price"> </p>
+                        <div class="product-image"></div>
+                        <p class="product-name"> </p>
+                        <p class="product-price"> </p>
                     </div>
                     <div className="visibility">
-                        <div className="product-image"></div>
-                        <p className="product-name"> </p>
-                        <p className="product-price"> </p>
+                        <div class="product-image"></div>
+                        <p class="product-name"> </p>
+                        <p class="product-price"> </p>
                     </div>
                     <div className="visibility">
-                        <div className="product-image"></div>
-                        <p className="product-name"> </p>
-                        <p className="product-price"> </p>
+                        <div class="product-image"></div>
+                        <p class="product-name"> </p>
+                        <p class="product-price"> </p>
                     </div>
                 </div>
-                <div className="product-grid">
-                    {productElements}
-                </div>
+                <Router>
+                    <Routes>
+                        <Route path="/" element={<ProductItem products={filteredProducts} />} />
+                        <Route path="/product/:id" element={<ProductDetail products={products} />} />
+                    </Routes>
+                </Router>
             </main>
         </div>
     );
