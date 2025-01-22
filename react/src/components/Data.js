5455cb59346f1c10cf6175d40e410446dcdc7c38
Jirathiphorn Rattanabut <151444848+SerFresh@users.noreply.github.com>
2025-01-23 00:00:43 +0700

    Update Data.js

--- react/src/components/Data.js
+++ react/src/components/Data.js
@@ -1,11 +1,11 @@
 const products = [
     {
-        id: 1, //ProductItem line-> 10 | ProductDetail line-> 6, 7
-        name: "Sakuma", //ProductItem line-> 12,14 | ProductDetail line-> 16,18
-        type: "A", //ProductItem line-> 10 
-        material: "silver", //ProductItem line-> 10
-        image: "images/Sakuma.png", //ProductItem line-> 12 | ProductDetail line-> 16
-        price: "21,000", //ProductItem line-> 15 | ProductDetail line-> 19
+        id: 1, 
+        name: "Sakuma", 
+        type: "A", 
+        material: "silver", 
+        image: "Sakuma.png", 
+        price: "21,000", 
         detail: "A letter is a message written for a variety of purposes, from friendly to formal. They can help maintain bonds between friends, especially if they’re far apart. Letters are also used by professionals to communicate their concerns. In some schools" // ProductDetail line-> 20
     },
     {
@@ -13,8 +13,17 @@ const products = [
         name: "Test",
         type: "B",
         material: "gold",
-        image: "images/testimg.jpg",
+        image: "testimg.jpg",
         price: "32,000",
+        detail: "A letter is a message written for a variety of purposes, from friendly to formal. They can help maintain bonds between friends, especially if they’re far apart. Letters are also used by professionals to communicate their concerns. In some schools"
+    }
+    {
+        id: 3,
+        name: "Cat",
+        type: "C",
+        material: "gold",
+        image: "kitty.jpg",
+        price: "52,000",
         detail: "A letter is a message written for a variety of purposes, from friendly to formal. They can help maintain bonds between friends, especially if they’re far apart. Letters are also used by professionals to communicate their concerns. In some schools"
     }
 ];
